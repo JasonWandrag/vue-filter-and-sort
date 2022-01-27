067d361306ac0b2ad7de1b7083a10aa7b79a4631
JasonWandrag <jasonwandrag@hotmail.com>
2022-01-27 08:26:16 +0200

    Created filtering for cards

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<template>
+  <div v-if="filteredPeople">
+    <div class="sortBar">
+      <label>
+        Sort Age:
+        <select v-model="ageSort" @change="sortAge(ageSort)">
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </label>
+      <label>
+        Sort Name:
+        <select v-model="nameSort" @change="sortName(nameSort)">
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </label>
+      <label>
+        Filter Gender:
+        <select v-model="gender" @change="filterGender(gender)">
+          <option value="">All</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </label>
+    </div>
+    <div id="user-list">
+      <Card
+        v-for="(person, idx) of filteredPeople"
+        :key="idx"
+        :person="person"
+        :idx="idx"
+      />
+    </div>
+  </div>
+  <div v-else>
+    <h2>Summoning Humans...</h2>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+
+export default {
+  name: "App",
+  components: { Card },
+  data() {
+    return {
+      people: null,
+      filteredPeople: null,
+      ageSort: "",
+      nameSort: "",
+      gender: "",
+    };
+  },
+  mounted() {
+    fetch("https://randomuser.me/api/?results=100")
+      .then((res) => res.json())
+      .then((data) => {
+        this.people = data.results;
+        this.filteredPeople = data.results;
+      })
+      .catch((err) => console.log(err));
+  },
+  methods: {
+    sortAge(dir) {
+      this.filteredPeople = this.filteredPeople.sort(
+        (a, b) => a.dob.age - b.dob.age
+      );
+      if (dir == "desc") this.filteredPeople.reverse();
+    },
+    sortName(dir) {
+      this.filteredPeople = this.filteredPeople.sort((a, b) => {
+        if (a.name.first.toLowerCase() < b.name.first.toLowerCase()) {
+          return -1;
+        }
+        if (a.name.first.toLowerCase() > b.name.first.toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      });
+      if (dir == "desc") this.filteredPeople.reverse();
+    },
+    filterGender(gender) {
+      if (gender) {
+        this.filteredPeople = this.people.filter(
+          (person) => person.gender == gender
+        );
+      } else {
+        this.filteredPeople = this.people;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+@import url(https://fonts.googleapis.com/css?family=Raleway:400,600);
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+body {
+  background: url(https://picsum.photos/1200);
+  background-size: cover;
+  background-attachment: fixed;
+  padding-top: 60px;
+}
+
+.sortBar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  background-color: white;
+  padding: 20px;
+}
+
+#user-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="card">
+    <div class="container">
+      <img :src="img" alt="cookies" class="hero-image" />
+
+      <div class="information">
+        <img
+          :src="person.picture.large"
+          :alt="person.name.first"
+          class="avatar"
+        />
+        <div class="name">{{ person.name.first }} {{ person.name.last }}</div>
+        <div class="position">{{ person.gender }}</div>
+
+        <div class="stats">
+          <span class="followers">
+            <span class="value">{{ person.dob.age }}</span>
+            <span class="label">age</span>
+          </span>
+
+          <span class="following">
+            <span class="value">{{ person.location.country }}</span>
+            <span class="label">country</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["person", "idx"],
+  mounted() {
+    console.log(this.person);
+  },
+  data() {
+    return {
+      img: "https://picsum.photos/300/?random=" + this.idx,
+    };
+  },
+};
+</script>
+<style>
+.card {
+  font: 13px/23px "Raleway", Arial, sans-serif;
+  color: #303336;
+  overflow: hidden;
+  margin: 20px;
+  background: rgba(255, 255, 255, 0.4);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+}
+
+.container {
+  margin: 0 auto;
+  width: 300px;
+  height: 465px;
+  border-radius: 3px;
+  position: relative;
+}
+
+.hero-image {
+  width: 100%;
+  height: 50%;
+  object-fit: cover;
+}
+
+.information {
+  text-align: center;
+  background: none;
+}
+
+.avatar {
+  margin: 0 auto;
+  margin: -82px auto 15px;
+  display: block;
+}
+
+.name {
+  font-size: 22px;
+}
+
+.position {
+  font-size: 16px;
+  color: #8c98a8;
+  margin-bottom: 24px;
+}
+
+.stats {
+  margin: auto;
+  border-top: 1px solid #ced5e0;
+  width: 240px;
+}
+.stats .followers,
+.stats .following,
+.stats .stories {
+  display: inline-block;
+  padding: 6px 10px 0;
+}
+.stats .followers {
+  border-right: 1px solid #ced5e0;
+}
+.stats .value {
+  font-size: 18px;
+  font-weight: 600;
+}
+.stats .label {
+  display: block;
+  font-size: 14px;
+  color: #8c98a8;
+}
+</style>
